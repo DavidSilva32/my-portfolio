6c7d4b1fa69cd3ea45d07522642dc79b01820f0e
DavidSilva32 <david.silvaxd32@gmail.com>
2023-05-05 06:35:02 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import MainContent from "./components/MainContent.jsx";
-import Sidebar from "./components/sidebar.jsx";
+import MainContent from "./components/MainContent";
+import Sidebar from "./components/Sidebar";
 
 import "./styles/components/app.sass";
 
